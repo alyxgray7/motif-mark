95013630960fa23bc6841e12b277fd98e62e53c1
Alyx Gray <agray11@MacBook-Pro.local>
2021-02-11 22:50:21 -0800

    update code

--- motif_mark.py
+++ motif_mark.py
@@ -120,12 +120,6 @@ def generate_motifs(motif_file):
     return motif_dict, motif_list
 motif_dict, motif_list = generate_motifs(motif_file)
 
-# Sanity check
-# for i, item in enumerate(motif_dict):
-#     print(i, len(motif_dict[item]))
-#print(motif_dict["yyyyyyyyyy"])
-#print(motif_list)
-
 
 def store_fasta_info(fasta_file):
     """
@@ -159,13 +153,12 @@ def store_fasta_info(fasta_file):
                 # Sum the number of nucleotides in sequence for each key
                 for seq in seq_list:
                     cnt += len(seq)
+
                     # Add all information to dictionary
                     fasta_dict[header] = [long_seq, cnt]
-                    #print(cnt)
 
     return fasta_dict
 fasta_dict = store_fasta_info(fasta_file)
-#print(fasta_dict,"\n\n")
 
 
 def get_int_ex_coordinates(fasta_dict):
@@ -212,7 +205,6 @@ def get_int_ex_coordinates(fasta_dict):
             
     return int_ex_coord_dict
 int_ex_coord_dict = get_int_ex_coordinates(fasta_dict)
-#print(int_ex_coord_dict,"\n\n")
 
 
 def get_motif_coordinates(motif_dict):
@@ -242,7 +234,6 @@ def get_motif_coordinates(motif_dict):
             combos = motif_dict[motif]
 
             # Loop through each combo 
-            #motif_pos = []
             for combo in combos:
 
                 # Create regex pattern based on unique motif combo
@@ -265,7 +256,6 @@ def get_motif_coordinates(motif_dict):
 
     return gene_motif_coord_dict
 motif_coord_dict = get_motif_coordinates(motif_dict)
-#print(motif_coord_dict)
 
 
 def name_image(file_name):
@@ -285,7 +275,6 @@ def name_image(file_name):
     image_name = str(image_name[0]) + ".svg"
     return image_name
 image_name = name_image(fasta_file)
-#print(image_name)
 
 
 def get_dims(fasta_dict):
@@ -294,7 +283,7 @@ def get_dims(fasta_dict):
     """
 
     lengths = []
-    scale_x = 7
+    scale_x = 6 ## may need to adjust this!!!
     scale_y = 100
     for seq_length in fasta_dict.values():
         lengths.append(seq_length[1])
@@ -302,7 +291,6 @@ def get_dims(fasta_dict):
         height = float((len(lengths) * scale_y) + 40) # Add extra space for legend at bottom
     return width, height
 WIDTH, HEIGHT = get_dims(fasta_dict)
-#print(WIDTH, HEIGHT)
 
 
 # Alignment coordinates
@@ -313,14 +301,12 @@ scale_y = 100 # each gene map should fit within this height
 
 # Generate color palette (Pete approved)
 num_colors = len(motif_dict)
-#print(num_colors)
 palette = sns.color_palette("colorblind", num_colors)
 
 # Set motifs to color
 motif_colors = {}
 for count, motif in enumerate(motif_list):
     motif_colors[motif] = palette[count]
-#print(motif_colors)
 
 
 ### Begin drawing image
@@ -340,31 +326,24 @@ with cairo.SVGSurface(image_name, WIDTH, HEIGHT) as surface:
 
         # set new y coordinate
         y1 = y0 + scale_y + (scale_y * i)
-        #print(y1)
         cr.move_to(x0,y1)
     
     # Set starting intron position for first gene
     y1 = y0 + (scale_y // 2.5)
-    #cr.move_to(x0, y1)
 
     # Loop through and draw intronic regions
     intron_y = 5 # Set height of intronic region
     for i, intron in enumerate(int_ex_coord_dict):
+        y1 = y0 + (scale_y // 2.5) + (100 * i)
+        cr.move_to(x0,y1)
         intron_coordinates = int_ex_coord_dict[intron][0]
         intron_x = max(intron_coordinates)
-        #print(intron_x)
         cr.rectangle(x0, y1, intron_x, intron_y)
         cr.fill()
         cr.set_source_rgb(0,0,0)
-        
-        # set new y coordinate
-        y1 = y1 + scale_y + (scale_y * i)
-        print(y1)
-        cr.move_to(x0, y1)
 
     # Draw exonic regions
     exon_y = intron_y * 7 # Set height of exonic region
-    #print(exon_y)
     for i, exon in enumerate(int_ex_coord_dict):
         exon_coordinates = int_ex_coord_dict[exon][1]
         exon0 = min(exon_coordinates) # exon start
@@ -397,12 +376,11 @@ with cairo.SVGSurface(image_name, WIDTH, HEIGHT) as surface:
     x3 = x0 + 20
     for i, colors in enumerate(motif_colors):
         cr.rectangle(x3, HEIGHT - 30, 20, 20)
-        cr.set_source_rgba(motif_colors[colors][0], motif_colors[colors][1], motif_colors[colors][2], .8)
+        cr.set_source_rgb(motif_colors[colors][0], motif_colors[colors][1], motif_colors[colors][2])
         cr.fill()
 
         # set new x coordinate
         x3 = x3 + 100 + (2 * i)
-        #print(x3)
         cr.move_to(x3, HEIGHT - 10)
     
     # Map motifs on gene
@@ -411,16 +389,14 @@ with cairo.SVGSurface(image_name, WIDTH, HEIGHT) as surface:
     # Loop through for each gene
     for gene_num, items in enumerate(motif_coord_dict):
         coord_dict = motif_coord_dict[items]
-        #print(coord_dict)
 
         # Get motifs and colors
         for motif_num, motifs in enumerate(coord_dict):
             colors = motif_colors[motifs]
             coordinates = coord_dict[motifs]
-            #print(motif_num, coord_dict[motifs], colors)
 
             # Set the color for each motif
-            cr.set_source_rgba(colors[0], colors[1], colors[2], 0.8)
+            cr.set_source_rgb(colors[0], colors[1], colors[2])
 
             # Mark motifs
             for coordinate in coordinates:
@@ -430,190 +406,7 @@ with cairo.SVGSurface(image_name, WIDTH, HEIGHT) as surface:
                 y3 = y0 + 25 + (100 * gene_num)
                 cr.rectangle(x4, y3, 2.5, motif_y)
                 cr.fill()
-                #print(gene_num, motif_num, coordinate, (x4,y3), colors)
 
     cr.show_page()
 print("Image Saved")
 
-
-
-
-
-
-
-    # # Split the motif list into sublists
-    # # Loop through dictionary to get motif coordinates
-    # for items in motif_coord_dict:
-    #     coordinates = motif_coord_dict[items]
-    #     print(coordinates)
-    #     check = coordinates[0]
-        
-    #     # Loop through coordinates
-    #     for coordinate in coordinates:
-
-    #         if coordinate >= check:
-    #             print(check, coordinate)
-            
-    #         else:
-    #             pass
-    #         #print(coordinate)
-    #     #print(check)
-
-        # Loop through coordinates
-        # check = 0
-        # for coordinate in coordinates:
-        #     check = coordinate
-
-        #     checklist.append(coordinate)
-        # print(coordinates)
-
-    # for items in motif_coord_dict:
-    #     coordinates = motif_coord_dict[items]
-    #     print(coordinates)
-        
-        # if motif_coord_dict[i] > motif_coord_dict[i -1]:
-        #     print(i)
-    # for motifs in motif_coord_dict:
-    #     motif_coordinates = (motif_coord_dict[motifs])
-    #     for coordinate in motif_coordinates:
-    #         print(motif_coordinates[coordinate])
-        #print(motif_coordinates)
-        # cutoff = motif_coordinates[-1]
-        # print(cutoff)
-        # for coordinate in motif_coordinates:
-        #     if motif_coordinates[coordinate] < 
-        #     counter = coordinate
-        #     print(counter)
-            # gene_counter = coordinate
-            # if coordinate 
-            # print(gene_counter, coordinate)
-    
-
-
-# for intron in int_ex_coord_dict:
-#     intron_coordinates = int_ex_coord_dict[intron][0]
-#     print(intron_coordinates)
-    
-
-# # Loop through and draw intronic regions
-# y2 = y0 + (90/2)
-# cr.move_to(x0,y2)
-# for introns in int_ex_coord_dict.values():
-
-# draw_gene()
-
-
-# for i, header in enumerate(int_ex_coord_dict):
-#     print(i) # position
-#     print(header) # header name
-
-# for positions in int_ex_coord_dict:
-#     print(int_ex_coord_dict[positions][1])
-#     # introns = po
-#     # exons = positions[1]
-#     # print(exons)
-
-# set width and height of image based on sequence lengths and #
-# SCALE = 9
-# WIDTH = (max(lengths) * 10)
-# HEIGHT = (len(lengths) * 10) * SCALE
-# X,Y = 10,10
-# SPACE = 30
-# INTRON_HEIGHT = 3.5
-# EXON_HEIGHT = 9
-# print(WIDTH, HEIGHT)
-
-# for i, length in enumerate(lengths):
-#     print(i)
-#     print(length)
-
-# # Create SVG surface
-# with cairo.SVGSurface("test.svg", WIDTH, HEIGHT) as surface:
-#     # start object for SVG surface
-#     context = cairo.Context(surface)
-
-#     # Loop through each read
-#     for read in int_ex_coord_dict:
-#         context.set_source_rgb(0,0,0) # black
-#         context.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
-#         context.set_font_size(12)
-#         context.move_to(X,Y)
-#         context.show_text(read)
-
-#         # Draw line for length of gene (intron)
-#         for i, length in enumerate(lengths):
-#             context.rectangle(X, Y + (SPACE + (SPACE * i)), (length * SCALE), INTRON_HEIGHT)
-#         #context.rectangle(X,Y+30,(max(lengths) * 9),3.5)
-#             context.set_source_rgb(0.35,0,0)
-#             context.fill()
-
-#         # Draw exons
-
-#         #context.rectangle()
-#         # context.rectangle(50, 50, 50, 50)
-#         # set color of context
-#         # Fill color inside
-
-#         context.stroke()
-# print("Image Saved")
-
-
-
-
-# def append_value(dict_obj, key, value):
-#     """
-#     Appends a key value to an existing key in the dictionary.
-#     """
-#     # Check if key exist in dict or not
-#     if key in dict_obj:
-#         # Key exist in dict.
-#         # Check if type of value of key is list or not
-#         if not isinstance(dict_obj[key], list):
-#             # If type is not list then make it list
-#             dict_obj[key] = [dict_obj[key]]
-#         # Append the value in list
-#         dict_obj[key].append(value)
-#     else:
-#         # As key is not in dict,
-#         # so, add key-value pair
-#         dict_obj[key] = value
-#     return 
-
-                # # combo letters
-                # # lowercase letters
-                # "a":["a","A"            ],
-                # "c":[    "c","C"        ],
-                # "g":[        "g","G"    ],
-                # "t":[            "t","T","u","U"],
-                # "u":[            "u","U","t","T"],
-                # "w":["a","A","t",        "T"],
-                # "s":[    "c","C","g","G"    ],
-                # "m":["a","A","c","C"        ],
-                # "k":[        "g","G","t","T"],
-                # "r":["a","A",    "g","G"   ],
-                # "y":[    "c","C"    "t","T"],
-                # "b":[    "c","C","g","G","t","T"],
-                # "d":["a","A"    "g","G","t","T"],
-                # "h":["a","A","c","C",    "t","T"],
-                # "v":["a","A","c","C","g","G"   ],
-                # "n":["a","A","c","C","g","G","t","T"],
-                # "z":[               ],
-                # # combo letters
-                # # uppercase letters
-                # "A":["a","A"            ],
-                # "C":[    "c","C"        ],
-                # "G":[        "g","G"    ],
-                # "T":[            "t","T","u","U"],
-                # "U":[            "u","U","t","T"],
-                # "W":["a","A","T",        "t"],
-                # "S":[    "c","C","g","G"    ],
-                # "M":["a","A","c","C"        ],
-                # "K":[        "g","G","t","T"],
-                # "R":["a","A",    "g","G"   ],
-                # "Y":[    "c","C"    "t","T"],
-                # "B":[    "c","C","g","G","t","T"],
-                # "D":["a","A"    "g","G","t","T"],
-                # "H":["a","A","c","C",    "t","T"],
-                # "V":["a","A","c","C","g","G"   ],
-                # "N":["a","A","c","C","g","G","t","T"],
-                # "Z":[               ]
